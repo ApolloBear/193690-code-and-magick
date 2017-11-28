94d181ee8cd45e17aba0843c915b0ce3fb54e4d4
ApolloBear <yxmbilka@gmail.com>
2017-11-28 11:17:06 +0300

    Новая версия, требуются доработки
    проблема с высотой и почему то цвета так же подключаются к тексту.

--- js/stat.js
+++ js/stat.js
@@ -1,32 +1,48 @@
-window.renderStatistics = function (ctx, names, times) {
+window.renderStatistics = function(ctx, names, times) {
+  var max = -1;
+  var maxIndex = -1;
+  var histogramHeight = 150; // px;
+  var step = histogramHeight / (max - 0); // px;
+  var barWidth = 40; // px;
+  var indent = 50; // px;
+  var initialX = 120; // px;
+  var initialY = 250; // px;
+  var initialTimes = 80; //px;
+  var lineHeight = 15; // px;
+  //Box
   ctx.fillRect(100, 10, 420, 270);
   ctx.fillStyle = "rgba(255, 255, 255, 1)";
-
+  //Shadow
   ctx.fillRect(90, 0, 420, 270);
   ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-
+  //Text
   ctx.font = "16px PT Mono";
   ctx.fillText("Ура вы победили!", 140, 40);
   ctx.fillText("Список результатов:", 140, 60);
-  names = ['Вы'];
-  times = [];
-
-  var max = -1;
-  var maxIndent = -1;
-
+  //
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
+      maxIndex = i;
     }
   }
 
-  var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
-
   for (var i = 0; i < times.length; i++) {
-    ctx.fillRect(120 + 50 * i, 250, 40, -times[i] * step);
-    ctx.fillText(names[i], 120 + 5 + 50 * i, 270);
-    ctx.fillText(times[i], 120 + 50 * i, 90);
+    Math.round(times[i]);
+    ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step); //fillRect(x, y, width, height)
+    if (names === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(255, 0, 0,' + Math.random(0, 1);
+    }
+    ctx.fillText(names[i], initialX + indent * i, initialY + lineHeight); //illText(text, x, y)
+    ctx.fillText(times[i], initialX + indent * i, initialTimes); //illText(text, x, y)
   }
 };
+
+// ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 60);
+
+// function getRandoomNumber(0, 1) {
+//   return MathRandom.....; // Разобраться
+// };
